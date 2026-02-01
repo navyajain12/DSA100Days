ed30d6e77e8d6bba33c75013cf3ff3d0ecf4681d
Navya <navyajain12002@gmail.com>
2026-02-01 12:00:00 +0530

    Day 1

--- script.sh
+++ script.sh
@@ -1,14 +1,22 @@
 start="2026-02-01"
 end="2026-04-30"
 
+i=1
 current="$start"
 
 while [ "$(date -d "$current" +%Y%m%d)" -le "$(date -d "$end" +%Y%m%d)" ]
 do
-  echo "Commit on $current" >> file.txt
+  # create file for each day
+  echo "Day $i commit" > "Day$i.txt"
 
-  GIT_AUTHOR_DATE="$current 12:00:00" GIT_COMMITTER_DATE="$current 12:00:00" git add .
-  GIT_AUTHOR_DATE="$current 12:00:00" GIT_COMMITTER_DATE="$current 12:00:00" git commit -m "Commit for $current"
+  # stage file
+  git add .
 
+  # commit with custom date
+  GIT_AUTHOR_DATE="$current 12:00:00" GIT_COMMITTER_DATE="$current 12:00:00" \
+  git commit -m "Day $i"
+
+  # next day
   current=$(date -d "$current + 1 day" +"%Y-%m-%d")
+  i=$((i+1))
 done
